60019b109bf9bee3cd90aa960b1bbffb7fbbdf03
Erasmus <erasmusthegeek@gmail.com>
2022-07-09 14:20:52 +0000

    Firstpush

--- ToBinary.py
+++ ToBinary.py
@@ -1,12 +1,34 @@
-number = input("Input a decimal number")
-copy = number
-number = int(number)
-bin_list = []
-while number > 0 :
-    num = number % 2
-    bin_list.append(num)
-    number //= 2
-bin_list.reverse()
-print("The binary equivalence of", copy,  "is: ")
-for i in bin_list :
-    print(i, end="")
+
+# FUNCTION: Convert binary to decimal
+def toBinary():
+    Value = input("Enter decimal number >>> :") #Take user input
+    copy = Value
+    number = int(Value)
+    bin_list = []
+    while number > 0:
+        num = number % 2
+        bin_list.append(num)
+        number //= 2
+    bin_list.reverse()
+    print("The binary equivalence of", copy, "is : ")
+    for i in bin_list:
+        print(i, end="")
+
+# function to convert binary to decimal
+def convertToDecimal():
+    binary = input('Enter Binary Number >>> : ')
+    decimal = 0
+    # lenght of binary
+    i = len(binary)
+
+    # loop through the binary digit
+    for x in binary:
+        i = i - 1
+        decimal += pow(2, 1) * int(x)
+    print("Decimal of {p} is {q} ".format(p=binary, q=decimal))
+
+toBinary()
+print()
+
+convertToDecimal()
+print()
