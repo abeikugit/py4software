6076538a4fad0049b046876c25aebb55b3ce1352
Emma Mozu <emmanuel.mozu@amalitech.org>
2022-07-16 14:25:42 +0000

    write anything

--- ToBinary.py
+++ ToBinary.py
@@ -1,14 +1,42 @@
-#!/usr/bin/python3
+# MID-SEMESTER 
+#EMMANUEL AGYABEN MOZU
+#BT/SEG/21/008
 
-number = input("Input a decimal number")
-copy = number
-number = int(number)
-bin_list = []
-while number > 0 :
-    num = number % 2
-    bin_list.append(num)
-    number //= 2
-bin_list.reverse()
-print("The binary equivalence of", copy,  "is: ")
-for i in bin_list :
-    print(i, end="")
+
+
+#convrt numbers to binary
+from ast import Pass
+
+
+def number_to_binary(digit):
+    digit = int(digit)
+    bin_list = []
+    while digit > 0 :
+        cast = digit % 2
+        bin_list.append(cast)
+        digit //= 2
+    bin_list.reverse()
+    print(" Binary equivalence of", copy,  "is: ")
+    for i in bin_list :
+        print(i, end="") 
+
+#convrt binary to number 
+def binary_to_number():
+        number = int(value)
+        bin_list = []
+        while number > 0:
+            num = number % 2
+            bin_list.append(num)
+            number //= 2
+        bin_list.reverse()
+        res = [str(i) for i in bin_list]
+        return "".join(res)
+
+
+
+
+digit = input("Please enter a decimal value:>")
+binary_to_number(digit)
+
+digit = input("Please enter a binary value:>")
+binary_to_number(digit)
