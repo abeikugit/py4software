417ca7d53983d2c15ccff21e55664038f7cf0bd5
EmmanuelMozu <emmanuel.mozu@amalitech.org>
2022-07-30 11:07:10 +0000

    Update ToBinary.py

--- ToBinary.py
+++ ToBinary.py
@@ -1,14 +1,26 @@
-#!/usr/bin/python3
-#Just for practice _Erasmus
-number = input("Input a decimal number")
-copy = number
-number = int(number)
+
+#
+# Mozu Agyaben Emmanuel
+# BT/SEG/21/008
+# QUESTION 3 
+
+print("\nQuestion 3.")
+digit = input("Please enter a decimal value:>")
+copy = digit
+digit = int(digit)
 bin_list = []
-while number > 0 :
-    num = number % 2
-    bin_list.append(num)
-    number //= 2
+while digit > 0 :
+    cast = digit % 2
+    bin_list.append(cast)
+    digit //= 2
 bin_list.reverse()
-print("The binary equivalence of", copy,  "is: ")
+print(" Binary equivalence of", copy,  "is: ")
 for i in bin_list :
     print(i, end="")
+
+print("\nQuestion 2.")
+# 2. create a program to accept the name of a user and display the name only if it is Palindrome1
+user_name = input("Enter your name: ")
+new_name = user_name[::-1]
+if user_name == new_name:print("Palindrome : ", user_name)
+
